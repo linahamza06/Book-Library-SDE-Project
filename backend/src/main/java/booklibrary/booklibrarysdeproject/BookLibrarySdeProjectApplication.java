c81952da7f0218eb23060b6f4208be0a2a7c2f40
Lina Hamza <lina.hamza006@gmail.com>
2025-11-20 20:49:12 +0100

    Initial commit

---
+++ backend/src/main/java/booklibrary/booklibrarysdeproject/BookLibrarySdeProjectApplication.java
@@ -0,0 +1,13 @@
+package booklibrary.booklibrarysdeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibrarySdeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibrarySdeProjectApplication.class, args);
+    }
+
+}
